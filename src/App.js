cc58c5a53cc6907ae03f9728fe80c77e5f972291
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-01-26 13:25:06 +0530

    advisory

--- src/App.js
+++ src/App.js
@@ -1,126 +1,105 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { HomePage } from "./pages/home/HomePage"
-import { AdvisoryBoard } from "./pages/about/AdvisoryBoard"
-import { Startups } from "./pages/about/Startups"
-import { Teams } from "./pages/about/Teams"
-import { Partners } from "./pages/about/Partners"
-import { Mentors } from "./pages/about/Mentors"
-import { VirtualTour } from "./pages/about/VirtualTour"
-import { IpSupport } from "./pages/initiatives/IpSupport"
-import { CoworkingSpace } from "./pages/initiatives/CoworkingSpace"
-import { MobilityTransportation } from "./pages/initiatives/centers/MobilityTransportation"
-import { RenewableEnergy } from "./pages/initiatives/centers/RenewableEnergy"
-import { IncubationAcceleration } from "./pages/initiatives/IncubationAcceleration"
-import { IncubationCollaboration } from "./pages/initiatives/IndustrialCollaboration"
-import { YoungMinds } from "./pages/initiatives/YoungMinds"
-import { UpcomingEvents } from "./pages/events/UpcomingEvents"
-import { PreviousEvents } from "./pages/events/PreviousEvents"
-import { Blog } from "./pages/media/Blog"
-import { AnnualReport } from "./pages/media/AnnualReport"
-import { MediaPresence } from "./pages/media/MediaPresence"
-import { Gallery } from "./pages/gallery/Gallery"
-import { SAINInvestors } from "./pages/captialInfusion/SAINInvestors"
-import { SAINStartups } from "./pages/captialInfusion/SAINStartups"
-import { Services } from "./pages/captialInfusion/Services"
-import { Entrepreneurship } from "./pages/initiatives/Entrepreneurship"
-import { PreIncubationProgram } from "./pages/initiatives/PreIncubationProgram"
-import { IncubationProgram } from "./pages/initiatives/IncubationProgram"
-import { Makerspace } from "./pages/initiatives/Makerspace"
-import { Contact } from "./pages/contact/Contact"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/home/HomePage";
+import { AdvisoryBoard } from "./pages/about/AdvisoryBoard";
+import { Startups } from "./pages/about/Startups";
+import { Teams } from "./pages/about/Teams";
+import { Partners } from "./pages/about/Partners";
+import { Mentors } from "./pages/about/Mentors";
+import { VirtualTour } from "./pages/about/VirtualTour";
+import { IpSupport } from "./pages/initiatives/IpSupport";
+import { CoworkingSpace } from "./pages/initiatives/CoworkingSpace";
+import { MobilityTransportation } from "./pages/initiatives/centers/MobilityTransportation";
+import { RenewableEnergy } from "./pages/initiatives/centers/RenewableEnergy";
+import { IncubationAcceleration } from "./pages/initiatives/IncubationAcceleration";
+import { IncubationCollaboration } from "./pages/initiatives/IndustrialCollaboration";
+import { YoungMinds } from "./pages/initiatives/YoungMinds";
+import { UpcomingEvents } from "./pages/events/UpcomingEvents";
+import { PreviousEvents } from "./pages/events/PreviousEvents";
+import { Blog } from "./pages/media/Blog";
+import { AnnualReport } from "./pages/media/AnnualReport";
+import { MediaPresence } from "./pages/media/MediaPresence";
+import { Gallery } from "./pages/gallery/Gallery";
+import { SAINInvestors } from "./pages/captialInfusion/SAINInvestors";
+import { SAINStartups } from "./pages/captialInfusion/SAINStartups";
+import { Services } from "./pages/captialInfusion/Services";
+import { Entrepreneurship } from "./pages/initiatives/Entrepreneurship";
+import { PreIncubationProgram } from "./pages/initiatives/PreIncubationProgram";
+import { IncubationProgram } from "./pages/initiatives/IncubationProgram";
+import { MakerSpace } from "./pages/initiatives/MakerSpace";
+import { Contact } from "./pages/contact/Contact";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-				{/* about tab */}
-				<Route
-					path="/about/advisoryboard"
-					element={<AdvisoryBoard />}
-				/>
-				<Route path="/about/startups" element={<Startups />} />
-				<Route path="/about/teams" element={<Teams />} />
-				<Route path="/about/partners" element={<Partners />} />
-				<Route path="/about/mentors" element={<Mentors />} />
-				<Route path="/about/virtualtour" element={<VirtualTour />} />
+        {/* about tab */}
+        <Route path="/about/advisoryboard" element={<AdvisoryBoard />} />
+        <Route path="/about/startups" element={<Startups />} />
+        <Route path="/about/teams" element={<Teams />} />
+        <Route path="/about/partners" element={<Partners />} />
+        <Route path="/about/mentors" element={<Mentors />} />
+        <Route path="/about/virtualtour" element={<VirtualTour />} />
 
-				{/* initiatives */}
-				<Route path="/initiatives/ipsupport" element={<IpSupport />} />
-				<Route
-					path="/initiatives/entrepreneurship"
-					element={<Entrepreneurship />}
-				/>
-				<Route
-					path="/initiatives/youngminds"
-					element={<YoungMinds />}
-				/>
-				<Route
-					path="/initiatives/preincubationprogram"
-					element={<PreIncubationProgram />}
-				/>
-				<Route
-					path="/initiatives/makerspace"
-					element={<Makerspace />}
-				/>
-				<Route
-					path="/initiatives/incubationprogram"
-					element={<IncubationProgram />}
-				/>
-				<Route
-					path="/initiatives/coworkingspace"
-					element={<CoworkingSpace />}
-				/>
-				<Route
-					path="/initiatives/incubationacceleration"
-					element={<IncubationAcceleration />}
-				/>
-				<Route
-					path="/initiatives/incubationcollaboration"
-					element={<IncubationCollaboration />}
-				/>
+        {/* initiatives */}
+        <Route path="/initiatives/ipsupport" element={<IpSupport />} />
+        <Route
+          path="/initiatives/entrepreneurship"
+          element={<Entrepreneurship />}
+        />
+        <Route path="/initiatives/youngminds" element={<YoungMinds />} />
+        <Route
+          path="/initiatives/preincubationprogram"
+          element={<PreIncubationProgram />}
+        />
+        <Route path="/initiatives/makerspace" element={<MakerSpace />} />
+        <Route
+          path="/initiatives/incubationprogram"
+          element={<IncubationProgram />}
+        />
+        <Route
+          path="/initiatives/coworkingspace"
+          element={<CoworkingSpace />}
+        />
+        <Route
+          path="/initiatives/incubationacceleration"
+          element={<IncubationAcceleration />}
+        />
+        <Route
+          path="/initiatives/incubationcollaboration"
+          element={<IncubationCollaboration />}
+        />
 
-				{/* Centers */}
-				<Route
-					path="/center/mobilitytransportation"
-					element={<MobilityTransportation />}
-				/>
-				<Route
-					path="/center/renewableenergy"
-					element={<RenewableEnergy />}
-				/>
+        {/* Centers */}
+        <Route
+          path="/center/mobilitytransportation"
+          element={<MobilityTransportation />}
+        />
+        <Route path="/center/renewableenergy" element={<RenewableEnergy />} />
 
-				{/* Events */}
-				<Route
-					path="/centers/upcomingevents"
-					element={<UpcomingEvents />}
-				/>
-				<Route
-					path="/centers/previousevents"
-					element={<PreviousEvents />}
-				/>
+        {/* Events */}
+        <Route path="/centers/upcomingevents" element={<UpcomingEvents />} />
+        <Route path="/centers/previousevents" element={<PreviousEvents />} />
 
-				{/* Media */}
-				<Route path="/media/blog" element={<Blog />} />
-				<Route path="/media/annualreport" element={<AnnualReport />} />
-				<Route
-					path="/media/mediapresence"
-					element={<MediaPresence />}
-				/>
+        {/* Media */}
+        <Route path="/media/blog" element={<Blog />} />
+        <Route path="/media/annualreport" element={<AnnualReport />} />
+        <Route path="/media/mediapresence" element={<MediaPresence />} />
 
-				{/* Gallery */}
-				<Route path="/gallery" element={<Gallery />} />
+        {/* Gallery */}
+        <Route path="/gallery" element={<Gallery />} />
 
-				{/* Contact */}
-				<Route path="/contact" element={<Contact />} />
+        {/* Contact */}
+        <Route path="/contact" element={<Contact />} />
 
-				{/* Captial Infusion */}
-				<Route path="/infusion/investors" element={<SAINInvestors />} />
-				<Route path="/infusion/startups" element={<SAINStartups />} />
-				<Route path="/infusion/services" element={<Services />} />
-			</Routes>
-		</Router>
-	)
+        {/* Captial Infusion */}
+        <Route path="/infusion/investors" element={<SAINInvestors />} />
+        <Route path="/infusion/startups" element={<SAINStartups />} />
+        <Route path="/infusion/services" element={<Services />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/about/AdvisoryBoard.jsx
+++ src/pages/about/AdvisoryBoard.jsx
@@ -1,9 +1,96 @@
-import React from "react"
+import React from "react";
+import Teamcard from "../../components/Teamcard";
 
 export const AdvisoryBoard = () => {
-	return (
-		<>
-			<h1>AdvisoryBoard</h1>
-		</>
-	)
-}
+  let message = `There are many variations of passages of Lorem Ipsum available but the \n majo`;
+  const advisors = [
+    {
+      name: "John Doe",
+      title: "CEO, ABC Inc.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+    {
+      name: "Jane Smith",
+      title: "CTO, XYZ Corp.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+    {
+      name: "John Doe",
+      title: "CEO, ABC Inc.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+    {
+      name: "Jane Smith",
+      title: "CTO, XYZ Corp.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+    {
+      name: "John Doe",
+      title: "CEO, ABC Inc.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+    {
+      name: "Jane Smith",
+      title: "CTO, XYZ Corp.",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
+  ];
+  return (
+    <section className="bg-white">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h2 className="section-title">The Advisors Behind SIIF</h2>
+            <p className="section-subtitle">{message}</p>
+          </div>
+          {/* <div className="col-sm-6 col-md-4">
+            <div className="team-item">
+              <img
+                src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg"
+                className="team-img"
+                alt="pic"
+              />
+              <h3>JOHNATHAN HAWKINS</h3>
+              <div className="team-info">
+                <p>Head of SEO</p>
+                <p>
+                  Johnathan is our co-founder and has developed search
+                  strategies for a variety of clients
+                </p>
+                <ul className="team-icon">
+                  <li>
+                    <a href="#" className="twitter">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                 
+                  <li>
+                    <a href="#" className="facebook">
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="linkedin">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div> */}
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
+            {advisors.map((advisor, index) => {
+              return <Teamcard advisor={advisor} key={index} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
