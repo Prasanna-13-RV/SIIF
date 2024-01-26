e8a8ce9cb1315f713375b4aac93a245bc25a9718
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-01-26 21:52:00 +0530

    Advisor & Startup

--- src/pages/about/AdvisoryBoard.jsx
+++ src/pages/about/AdvisoryBoard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import Teamcard from "../../components/About.jsx/Teamcard";
+import Teamcard from "../../components/About.jsx/StartupCard";
 
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export const AdvisoryBoard = () => {
   let message = `There are many variations of passages of Lorem Ipsum available but the \n majo`;
@@ -8,22 +11,27 @@ export const AdvisoryBoard = () => {
     {
       name: "John Doe",
       title: "CEO, ABC Inc.",
-      image:"https://img.freepik.com/premium-photo/contemplative-expression-serene-contemplative-gaze_894855-4132.jpg"
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
     },
     {
       name: "Jane Smith",
       title: "CTO, XYZ Corp.",
-      image:"https://img.freepik.com/premium-photo/contemplative-expression-serene-contemplative-gaze_894855-4132.jpg"
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
     },
     {
       name: "John Doe",
       title: "CEO, ABC Inc.",
-      image:"https://img.freepik.com/premium-photo/contemplative-expression-serene-contemplative-gaze_894855-4132.jpg"
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
     },
     {
       name: "Jane Smith",
       title: "CTO, XYZ Corp.",
-      image:"https://img.freepik.com/premium-photo/contemplative-expression-serene-contemplative-gaze_894855-4132.jpg"    },
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww",
+    },
     {
       name: "John Doe",
       title: "CEO, ABC Inc.",
@@ -41,15 +49,27 @@ export const AdvisoryBoard = () => {
     <section className="bg-white">
       <div className="container">
         <div className="row">
-          <div className="col-md-12 text-center">
-            <h2 className="section-title">The Advisors Behind SIIF</h2>
+          <div className="col-md-12 text-center pt-7">
+            <h2 className="text-4xl font-semibold">The Advisors Behind SIIF</h2>
             <p className="section-subtitle">{message}</p>
           </div>
-          <div class="container m-auto grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {advisors.map((advisor, index) => {
-              return <Teamcard advisor={advisor} key={index} />;
-            })}
-          </div>
+
+          <Row xs={1} md={2} lg={3} className="g-6 p-[2%]">
+            {advisors.map((advisor, idx) => (
+              <Col
+                key={idx}
+                className="p-6 transition duration-300 transform hover:scale-105"
+              >
+                <Card className="hover:border-neutral-400 hover:shadow-md">
+                  <Card.Img variant="top" src={advisor.image} />
+                  <Card.Body>
+                    <Card.Title>{advisor.name}</Card.Title>
+                    <Card.Text>{advisor.title}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </div>
       </div>
     </section>
--- src/pages/about/Teams.jsx
+++ src/pages/about/Teams.jsx
@@ -3,7 +3,22 @@ import React from "react"
 export const Teams = () => {
 	return (
 		<>
-			<h1>Teams</h1>
+		 <section className="bg-white">
+		 <img
+        className="w-screen h-[80vh]"
+        src="https://t4.ftcdn.net/jpg/03/54/00/91/240_F_354009182_Cc6DzlgU402u1bXAGyvXAbmp5EgYAV9D.jpg"
+        alt=""
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12  pt-7 bg-[]">
+            <h2 className="text-4xl font-semibold">The Team</h2>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
 		</>
 	)
 }
