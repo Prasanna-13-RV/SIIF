5bce20d9cfc3bd5a7a62d279c68f172b2d497893
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-01-27 21:03:19 +0530

    Team

--- src/pages/about/Teams.jsx
+++ src/pages/about/Teams.jsx
@@ -1,24 +1,37 @@
-import React from "react"
+import React from "react";
+import TeamSlider from "../../components/About.jsx/TeamSlider";
 
 export const Teams = () => {
-	return (
-		<>
-		 <section className="bg-white">
-		 <img
-        className="w-screen h-[80vh]"
-        src="https://t4.ftcdn.net/jpg/03/54/00/91/240_F_354009182_Cc6DzlgU402u1bXAGyvXAbmp5EgYAV9D.jpg"
-        alt=""
-      />
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12  pt-7 bg-[]">
-            <h2 className="text-4xl font-semibold">The Team</h2>
-
+  const Teammembers = [
+    {
+      name: "John Doe",
+      title: "CEO & Founder",
+      image:
+        "https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ET3.jpg",
+      linkedin: "https://www.linkedin.com",
+    },
+    {
+      name: "John Doe",
+      title: "CEO & Founder",
+      image:
+        "https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/ET3.jpg",
+      linkedin: "https://www.linkedin.com",
+    },
+  ];
+  return (
+    <>
+      <section className="bg-white">
+        <img
+          className="w-screen h-[60vh] sm:h-[80vh]"
+          src="https://t4.ftcdn.net/jpg/03/54/00/91/240_F_354009182_Cc6DzlgU402u1bXAGyvXAbmp5EgYAV9D.jpg"
+          alt=""
+        />
+        <div className="container pt-5">
+          <div className="row ">
+              <TeamSlider Teammembers={Teammembers} />
           </div>
-
         </div>
-      </div>
-    </section>
-		</>
-	)
-}
+      </section>
+    </>
+  );
+};
