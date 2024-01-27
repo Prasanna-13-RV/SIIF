d2efd7cb4a95e65593a36572fc8e7b97827812e4
Lohith Srk <srklohith05@gmail.com>
2024-01-27 21:27:07 +0530

    Initial commit of gallery

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,5 +1,12 @@
 import React from "react"
+import Card from '../../components/gallery/Card.component'
 
 export const Gallery = () => {
-	return <h1>Gallery</h1>
+	return <div className='px-10 lg:px-14 xl:px-16 my-5 grid grid-cols-3 gap-10'>
+		<Card/>
+		<Card/>
+		<Card/>
+		<Card/>
+		<Card/>
+	</div>
 }
