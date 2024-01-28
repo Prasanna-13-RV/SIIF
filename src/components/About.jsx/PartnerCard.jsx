3673160efd680bb7fc42c2aedc1ba85704323b1d
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-01-28 11:50:27 +0530

    Bhuvana

---
+++ src/components/About.jsx/PartnerCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PartnerCard = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default PartnerCard
--- src/pages/about/Partners.jsx
+++ src/pages/about/Partners.jsx
@@ -1,5 +1,19 @@
-import React from "react"
+import React from "react";
+import PartnerCard from "../../components/About.jsx/PartnerCard";
 
 export const Partners = () => {
-	return <h1>Partners</h1>
-}
+  return (
+    <>
+      <section className="bg-[#f7f7f7]">
+        <div className="container pt-5">
+          <div className="row ">
+            <div className="col-md-12 text-left pt-9">
+              <h2 className="text-5xl font-semibold pb-5">StartUps</h2>
+            </div>
+			<PartnerCard/>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
