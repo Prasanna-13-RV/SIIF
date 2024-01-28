51dd5b025a411c1b0448f6e62988a7682ecd0202
solomonsanthosh <solosanty2064@gmail.com>
2024-01-28 13:22:08 +0530

    SS

--- src/pages/captialInfusion/Services.jsx
+++ src/pages/captialInfusion/Services.jsx
@@ -4,68 +4,68 @@ export const Services = () => {
 	return <div className="w-full bg-[#1D293E] ">
 		<h1 className="py-16 font-bold text-3xl text-center text-white">Our Services</h1>
 		<div className=" grid  grid-cols-3 gap-5 pb-16 px-24">
-		<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
 			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
-			Deal Sourcing and Screening</h1>
-			<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
-				We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
-				curated selection of
-				high-potential ventures.
-			</p></div>
-	</div></div> 
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
+			<div className="border-2 border-white p-10 rounded-sm"><h1 className="text-white font-bold mb-6">
+				Deal Sourcing and Screening</h1>
+				<p className="text-white">RAIN actively source and evaluate potential investment opportunities.
+					We identify promising Start-Ups through various channels, including referrals, partnerships, and their own network connections. Our service ensures that investors have access to a
+					curated selection of
+					high-potential ventures.
+				</p></div>
+		</div></div>
 }
