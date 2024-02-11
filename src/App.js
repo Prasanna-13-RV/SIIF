fba0611a35a1977092b0e02051b32a6891b02f14
Prasanna <prasannavelmurugan0200@gmail.com>
2024-02-11 16:19:48 +0530

    rv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import Footer from './components/common/footer/Footer.component'
-import Navbar from './components/common/navbar/Navbar.component'
+import Footer from "./components/common/footer/Footer.component"
+import Navbar from "./components/common/navbar/Navbar.component"
 import "./index.css"
 import { AdvisoryBoard } from "./pages/about/AdvisoryBoard"
 import { Mentors } from "./pages/about/Mentors"
@@ -41,36 +41,54 @@ import { RenewableEnergy } from "./pages/initiatives/centers/RenewableEnergy"
 import { AnnualReport } from "./pages/media/AnnualReport"
 import { Blog } from "./pages/media/Blog"
 import { MediaPresence } from "./pages/media/MediaPresence"
+import Student from "./pages/apply/Student"
+import Faculty from "./pages/apply/Faculty"
+import Technopreneur from "./pages/apply/Technopreneur"
+import Mentor from "./pages/apply/Mentor"
 
 function App() {
 	return (
-		<div className='mt-10 lg:mt-36'>
-
+		<div className="mt-36">
 			<Router>
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 
 					{/* about tab */}
-					<Route path="/about/advisoryboard" element={<AdvisoryBoard />} />
+					<Route
+						path="/about/advisoryboard"
+						element={<AdvisoryBoard />}
+					/>
 					<Route path="/about/startups" element={<Startups />} />
 					<Route path="/about/teams" element={<Teams />} />
 					<Route path="/about/partners" element={<Partners />} />
 					<Route path="/about/mentors" element={<Mentors />} />
-					<Route path="/about/virtualtour" element={<VirtualTour />} />
+					<Route
+						path="/about/virtualtour"
+						element={<VirtualTour />}
+					/>
 
 					{/* initiatives */}
-					<Route path="/initiatives/ipsupport" element={<IpSupport />} />
+					<Route
+						path="/initiatives/ipsupport"
+						element={<IpSupport />}
+					/>
 					<Route
 						path="/initiatives/entrepreneurship"
 						element={<Entrepreneurship />}
 					/>
-					<Route path="/initiatives/youngminds" element={<YoungMinds />} />
+					<Route
+						path="/initiatives/youngminds"
+						element={<YoungMinds />}
+					/>
 					<Route
 						path="/initiatives/preincubationprogram"
 						element={<PreIncubationProgram />}
 					/>
-					<Route path="/initiatives/makerspace" element={<MakerSpace />} />
+					<Route
+						path="/initiatives/makerspace"
+						element={<MakerSpace />}
+					/>
 					<Route
 						path="/initiatives/incubationprogram"
 						element={<IncubationProgram />}
@@ -88,21 +106,35 @@ function App() {
 						element={<IncubationCollaboration />}
 					/>
 
+					{/* Events */}
+					<Route
+						path="/events/upcomingevents"
+						element={<UpcomingEvents />}
+					/>
+					<Route
+						path="/events/previousevents"
+						element={<PreviousEvents />}
+					/>
 					{/* Centers */}
 					<Route
 						path="/center/mobilitytransportation"
 						element={<MobilityTransportation />}
 					/>
-					<Route path="/center/renewableenergy" element={<RenewableEnergy />} />
-
-					{/* Events */}
-					<Route path="/centers/upcomingevents" element={<UpcomingEvents />} />
-					<Route path="/centers/previousevents" element={<PreviousEvents />} />
+					<Route
+						path="/center/renewableenergy"
+						element={<RenewableEnergy />}
+					/>
 
 					{/* Media */}
 					<Route path="/media/blog" element={<Blog />} />
-					<Route path="/media/annualreport" element={<AnnualReport />} />
-					<Route path="/media/mediapresence" element={<MediaPresence />} />
+					<Route
+						path="/media/annualreport"
+						element={<AnnualReport />}
+					/>
+					<Route
+						path="/media/mediapresence"
+						element={<MediaPresence />}
+					/>
 
 					{/* Gallery */}
 					<Route path="/gallery" element={<Gallery />} />
@@ -112,8 +144,14 @@ function App() {
 
 					{/* Captial Infusion */}
 					<Route path="/infusion" element={<Index />} />
-					<Route path="/infusion/investors" element={<SAINInvestors />} />
-					<Route path="/infusion/startups" element={<SAINStartups />} />
+					<Route
+						path="/infusion/investors"
+						element={<SAINInvestors />}
+					/>
+					<Route
+						path="/infusion/startups"
+						element={<SAINStartups />}
+					/>
 					<Route path="/infusion/services" element={<Services />} />
 
 					{/* Benefits Captial */}
@@ -143,7 +181,10 @@ function App() {
 						path="/benefits/trade/globalconnect"
 						element={<GlobalMarketConnect />}
 					/>
-					<Route path="/benefits/trade/demoday" element={<DemoDay />} />
+					<Route
+						path="/benefits/trade/demoday"
+						element={<DemoDay />}
+					/>
 					<Route
 						path="/benefits/trade/launchpad"
 						element={<LaunchPad />}
@@ -152,12 +193,20 @@ function App() {
 						path="/benefits/trade/startuptools"
 						element={<StartupTools />}
 					/>
+
+					{/* apply */}
+					<Route path="/apply/student" element={<Student />} />
+					<Route path="/apply/faculty" element={<Faculty />} />
+					<Route
+						path="/apply/technopreneur"
+						element={<Technopreneur />}
+					/>
+					<Route path="/apply/mentor" element={<Mentor />} />
 				</Routes>
 				<Footer />
 			</Router>
 		</div>
-
-	);
+	)
 }
 
-export default App;
+export default App
