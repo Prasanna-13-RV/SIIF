b99e62964b35e44ca3ad3d58994887f10b2d6355
solomonsanthosh <solosanty2064@gmail.com>
2024-02-17 00:12:38 -0800

    ss

--- src/pages/apply/Faculty.jsx
+++ src/pages/apply/Faculty.jsx
@@ -11,45 +11,45 @@ const Faculty = () => {
 
       <form className="m-16 w-[80%] lg:w-[50%] mx-auto flex flex-wrap justify-between items-start">
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">
 
             Name of the faculty*</label>
-          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="" required />
         </div>
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name of the Institution*</label>
-          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Name of the Institution*</label>
+          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="" required />
         </div>
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Id*
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Email Id*
           </label>
-          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="" required />
         </div>
 
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile no*
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Mobile no*
           </label>
-          <input type="tel" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="tel" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
         </div>
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Area of Interest*(3D Printing,IoT,Drone,Virtual Reality,Robotics
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Area of Interest*(3D Printing,IoT,Drone,Virtual Reality,Robotics
           </label>
-          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
         </div>
 
         <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Describe your startup Idea*
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Describe your startup Idea*
 
           </label>
-          <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>  </div>
+          <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder=""></textarea>  </div>
 
         <div className="mb-3 lg:mb-5 w-[100%]">
-          <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">
             Why do you want to join us as an Incubatee?*
           </label>
-          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+          <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5   " placeholder="" required />
         </div>
-        <button type="submit" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        <button type="submit" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
       </form>
 
     </div>
--- src/pages/apply/Mentor.jsx
+++ src/pages/apply/Mentor.jsx
@@ -1,3 +1,5 @@
+
+
 import React from 'react'
 
 const Mentor = () => {
@@ -19,54 +21,54 @@ const Mentor = () => {
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
 
 
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">
 
                         Full Name*</label>
-                    <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                    <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
                 </div>
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Id*
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Email Id*
                     </label>
-                    <input type="email" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                    <input type="email" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
                 </div>
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mobile no*
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Mobile no*
                     </label>
-                    <input type="tel" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                    <input type="tel" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="" required />
                 </div>
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Domain*
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Domain*
                     </label>
 
-                    <select id="years" size="3" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select id="years" size="3" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         <option>2016</option>
                         <option>2017</option>
                         <option>2018</option>
                         <option>2019</option>
                     </select>  </div>
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">LinkedIn Profile*</label>
-                    <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required />
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">LinkedIn Profile*</label>
+                    <input type="text" className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="" required />
                 </div>
 
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
-                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 dark:text-white">Why are you interested*
+                    <label className="w-full block mb-2 text-sm font-medium text-gray-900 ">Why are you interested*
 
                     </label>
-                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>  </div>
+                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder=""></textarea>  </div>
 
 
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
 
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 " for="file_input">
                         Upload your CV*</label>
-                    <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+                    <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none " id="file_input" type="file" />
 
                 </div>
                 <div className="mb-3 lg:mb-5 w-full lg:w-[45%]">
 
                 </div>
-                <button type="submit" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
             </form>
 
         </div>
