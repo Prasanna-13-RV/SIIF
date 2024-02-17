5e420d5a43991e8c52a7fd2b27680974442a2540
Prasanna <prasannavelmurugan0200@gmail.com>
2024-02-17 23:00:14 +0530

    rv

--- src/pages/about/VirtualTour.jsx
+++ src/pages/about/VirtualTour.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from "react"
+import { Helmet } from "react-helmet"
+import { Page404 } from "../../components/common/extra/Page404"
+
 export const VirtualTour = () => {
-  return (
-    <>
-      <Helmet>
-        <title>About | VirtualTour</title>
-      </Helmet>
-      <h1>VirtualTour</h1>
-    </>
-  );
-};
+	return (
+		<>
+			<Helmet>
+				<title>About | VirtualTour</title>
+			</Helmet>
+			{/* <h1>VirtualTour</h1> */}
+			<Page404 />
+		</>
+	)
+}
