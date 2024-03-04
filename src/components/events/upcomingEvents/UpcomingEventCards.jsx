52f0569b319ec2692492051cbad441c5f14e8bed
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-03-04 18:05:25 +0530

    adding upcoming events

--- src/components/events/upcomingEvents/UpcomingEventCards.jsx
+++ src/components/events/upcomingEvents/UpcomingEventCards.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import { UpcomingEventCard } from "../UpcomingEventCard"
 import image1 from "../../../img/events/upcoming_event1.jpg"
 import image2 from "../../../img/events/upcoming_event2.jpg"
+import image3 from "../../../img/events/upcoming_event3.jpg"
+
 
 export const UpcomingEventCards = () => {
 	return (
@@ -17,6 +19,14 @@ export const UpcomingEventCards = () => {
 			<UpcomingEventCard
 				link="https://www.instagram.com/p/C3ZzIbsBeef/"
 				image={image1}
+				// title={"Noteworthy technology acquisitions 2021"}
+				// description={
+				// 	"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order"
+				// }
+			/>
+			<UpcomingEventCard
+				link="https://forms.gle/fawkhYxf5YYJm1R98"
+				image={image3}
 				// title={"Noteworthy technology acquisitions 2021"}
 				// description={
 				// 	"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order"
